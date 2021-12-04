814e5207e71fe089c953504f36c3c72a6fa8cab7
Andrew Holmes <sherlockson17@gmail.com>
2021-12-04 16:24:49 -0500

    Add project files.

---
+++ WPCSCodingClubProject1/Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("1. Add \n" +
+            "2. Subtract");
+        string result = Console.ReadLine();
+        int selection = int.Parse(result);
+
+        if (selection == 1)
+        {
+            Console.WriteLine("Add the code to take in two numbers and add them here!");
+        }
+        else if (selection == 2)
+        {
+            Console.WriteLine("Add the code to take in two numbers and subtract them here!");
+        }
+    }
+}
